60aa4a28c7970b89352fe08ddfac3818dd561ff3
OrygnsCode <gibbidaniel@gmail.com>
2025-11-25 22:09:59 -0600

    fix: sort projects alphabetically

--- js/app.js
+++ js/app.js
@@ -12,6 +12,10 @@ async function loadProjects() {
     try {
         const response = await fetch('projects.json');
         const projects = await response.json();
+
+        // Sort projects alphabetically by title
+        projects.sort((a, b) => a.title.localeCompare(b.title));
+
         renderProjects(projects);
         setupFilters(projects);
     } catch (error) {
